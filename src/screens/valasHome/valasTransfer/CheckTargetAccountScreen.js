e2e24fc3b682e2f0022b35459df3828ae665657f
arfiandi16 <alibiabu16@gmail.com>
2024-06-14 20:21:59 +0700

    Merged Branch farrel and arfi

--- src/screens/valasHome/valasTransfer/CheckTargetAccountScreen.js
+++ src/screens/valasHome/valasTransfer/CheckTargetAccountScreen.js
@@ -299,7 +299,7 @@ const styles = StyleSheet.create({
   topContainer: {
     width: "100%",
     flex: 0.1,
-    marginTop: "12%",
+    marginTop: "10%",
     paddingHorizontal: 20,
   },
   middleContainer: {
